4770bb5d2866d966459bc63fe0e107819752ac00
data-scientist792 <piyush318@gmail.com>
2023-07-03 14:26:08 +0530

    First commit

---
+++ mongodDbFastApidemo/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+import routes
+from routes import router as list_router
+
+config = dotenv_values("keys.env")
+
+
+app = FastAPI()
+
+app.include_router(list_router, tags=["list"], prefix="/list")
+
+
+@app.get("/")
+async def root():
+    return {"message": "API using Fast API and pymongo"}
+
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["MONGODB_CONNECTION_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
